1893369375e9939a06c33d726af89e7da2f260ba
Andrew Smith <andrew@goaway>
2015-03-27 01:02:18 -0400

    Version 0.1 of cc-xmp-tag.pl Reading and writing works but will likely need to make some changes in how it works.

--- cc-xmp-tag/perl/cc-xmp-tag.pl
+++ cc-xmp-tag/perl/cc-xmp-tag.pl
@@ -1,1 +1,183 @@
+#!/usr/bin/perl -w
 
+# cc-xmp-tag.pl
+#
+# This program is a part of the cc-xmp-tag set of tools. Run it with 
+# no parameters to get help.
+# 
+# Author: Andrew Smith ( http://littlesvr.ca )
+# Version 0.1 changes:
+# - Initial version.
+# 
+# This program is distributed only under Version 2, June 1991 of the GPL, not
+# any later versions. Contact me if that's a problem for you for some reason.
+#
+
+eval "use Image::ExifTool; 1" || die "Couldn't load the ExifTool perl module. Please make sure Image::ExifTool is installed.\n";
+
+my @supportedTags = (
+    "AttributionName", 
+    "AttributionURL", 
+    "License", 
+    "Marked", 
+    "Title", 
+    "UsageTerms"
+);
+my %licenceURLs = (
+    "BY" => "http://creati-ecommons.org/licenses/by/4.0/",
+    "BY-NC" => "http://creativecommons.org/licenses/by-nc/4.0/", 
+    "BY-NC-ND" => "http://creativecommons.org/licenses/by-nd/4.0/", 
+    "BY-NC-SA" => "http://creativecommons.org/licenses/by-nc-sa/4.0/",
+    "BY-ND" => "http://creativecommons.org/licenses/by-nd/4.0/", 
+    "BY-SA" => "http://creativecommons.org/licenses/by-sa/4.0/"
+);
+my %licenceNames = (
+    "BY" => "Attribution 4.0 International",
+    "BY-NC" => "Attribution-NonCommercial 4.0 International", 
+    "BY-NC-ND" => "Attribution-NonCommercial-NoDerivs 4.0 International", 
+    "BY-NC-SA" => "Attribution-NonCommercial-ShareAlike  4.0 International",
+    "BY-ND" => "Attribution-NoDerivs 4.0 International", 
+    "BY-SA" => "Attribution-ShareAlike 4.0 International"
+);
+
+if (@ARGV < 3) {
+    die(getUsageString());
+}
+
+my $readOrWrite = shift;
+my $filename = shift;
+
+if ($readOrWrite eq "--read") {
+    # What tags to go through?
+    my @tagNames = ();
+    if ($ARGV[0] eq "ALL") {
+        @tagNames = @supportedTags;
+    }
+    else {
+        @tagNames = @ARGV;
+    }
+    
+    # Open the file and get the metadata from it
+    my $exifTool = new Image::ExifTool;
+    $success = $exifTool->ExtractInfo($filename);
+    if (!$success) {
+        die "Failed to extract info from file '$filename'\n";
+    }
+    
+    # Try to get the values for each tag requested on the commandline
+    foreach (@tagNames) {
+        my $tagName = $_;
+        if ( !grep(/^$tagName$/, @supportedTags) ) {
+            die "Asked to read unsupported tag '$tagName'\n";
+        }
+        my $value = $exifTool->GetValue($tagName);
+        if (defined($value)) {
+            print $value . "\n";
+        }
+        else {
+            print "No value for tag '$tagName'\n";
+        }
+    }
+}
+elsif ($readOrWrite eq "--write") {
+    # Parse out the tags and values requested
+    my %tagsAndValues = ();
+    foreach (@ARGV) {
+        # This "2" makes sure that an '=' can be used in the value safely
+        @pair = split('=', $_, 2);
+        if(scalar @pair != 2) {
+            die "Invalid argument: '$_'";
+        }
+        $tagsAndValues{$pair[0]} = $pair[1];
+    }
+    
+    # Open the file and get the metadata from it
+    my $exifTool = new Image::ExifTool;
+    $success = $exifTool->ExtractInfo($filename);
+    if (!$success) {
+        die "Failed to extract info from file '$filename'\n";
+    }
+    
+    # Write all the new tags
+    my $wroteSomething = 0;
+    my $licenceCodeUsed;
+    while ( my ($tag, $value) = each(%tagsAndValues) ) {
+        # If licence code specified: translate value to URL
+        if ($tag eq "License") {
+            $licenceCodeUsed = $value;
+            $value = $licenceURLs{"$value"};
+            if (!$value) {
+                die "Bad value for key 'License'\n";
+            }
+        }
+        ($success, $errStr) = $exifTool->SetNewValue($tag, $value);
+        if (!$success) {
+            die "Failed to set tag '$tag': " . $errStr;
+        }
+        $wroteSomething = 1;
+    }
+    
+    # !!Assume not public domain, perhaps rethink this later
+    if ($wroteSomething) {
+        ($success, $errStr) = $exifTool->SetNewValue("Marked", "True");
+        if (!$success) {
+            die "Failed to set tag 'Marked': " . $errStr;
+        } 
+    }
+    # Generate UsageTerms automatically, to looks something like this:
+    # This work is licensed under a &lt;a rel=&#34;license&#34; href=&#34;http://creativecommons.org/licenses/by/4.0/&#34;&gt;Creative Commons Attribution 4.0 International License&lt;/a&gt;.
+    if ($licenceCodeUsed) {
+        my $usageTerms = "This work is licensed under a &lt;a rel=&#34;license&#34; href=&#34;";
+        $usageTerms .= $licenceURLs{"$licenceCodeUsed"};
+        $usageTerms .= "&#34;&gt;Creative Commons ";
+        $usageTerms .= $licenceNames{"$licenceCodeUsed"};
+        $usageTerms .= " License&lt;/a&gt;.";
+        ($success, $errStr) = $exifTool->SetNewValue("UsageTerms", $usageTerms);
+        if (!$success) {
+            die "Failed to set tag 'UsageTerms': " . $errStr;
+        } 
+    }
+    
+    $exifTool->WriteInfo($filename);
+}
+else {
+    printUsage();
+}
+
+sub getUsageString {
+    my $message = <<END_MESSAGE
+Usage:
+  cc-xmp-tag.pl --read FILENAME TAG
+  cc-xmp-tag.pl --read FILENAME TAG1 TAG2 ...
+  cc-xmp-tag.pl --read FILENAME ALL
+  cc-xmp-tag.pl --write FILENAME TAG1='VALUE1' TAG2='VALUE2' ...
+Supported tags:
+END_MESSAGE
+;
+    $message .= " ";
+    foreach (@supportedTags) {
+        $message .= " " . $_;
+    }
+    $message .= "\nSupported licences (all are version 4.0): \n ";
+    foreach (sort keys %licenceURLs) {
+        $message .= " " . $_;
+    }
+    $message .= "\n";
+    
+    $message .= <<END_MESSAGE
+Examples:
+  cc-xmp-tag.pl --read test.jpg License
+  cc-xmp-tag.pl --read test.jpg ALL
+  cc-xmp-tag.pl --write test.jpg AttributionName='Andrew Smith' AttributionURL='http://littlesvr.ca' Title='My Title' License='BY-SA'
+Notes:
+  * Reading more than one tag at once prints the value of each tag on a new line. This is unlikely to be useful in a script.
+  * When writing don't try to use license URLs, use the short code instead.
+  * There's no need to write values for the 'Marked' and 'UsageTerms' tags because these are written automatically.
+END_MESSAGE
+;
+    return $message;
+}
+
+sub printUsage {
+    print getUsageString();
+}
